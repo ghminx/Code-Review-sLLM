2bca7de4cc821e543e7a8c2ebec412a8bab77678
ghminx <casu106@naver.com>
2025-03-02 12:56:21 +0900

    Change code review results to extract real-time text

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 from code_review_llm import code_review, translate_deepl
 import os
+import time
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
 
-
+def stream_data(text):
+    for line in text.split("\n"):
+        yield line
+        time.sleep(0.1)
+        
+        
 st.title('Code Review')
 
 code_input = st.text_area("enter the code:", height=300)
@@ -15,10 +21,19 @@ model_name = st.selectbox("Model", ["yi-coder:1.5b", "qwen2.5-coder","deepseek-c
 if st.button("Run"):
     if code_input.strip():
         with st.spinner("Running Code Review..."):
+            
             result = code_review(model_name, code_input)
             translated_text = translate_deepl(result, DEEPL_API_KEY)
         
-        st.markdown("### Code Review Result")
-        st.markdown(translated_text)
+            st.markdown("### Code Review Result")
+            output_area = st.empty()  
+
+            full_text = "" 
+
+            for chunk in stream_data(translated_text):
+                full_text += chunk + "\n"  
+                output_area.markdown(full_text)  
+
+        
     else:
         st.warning("please enter the code")
